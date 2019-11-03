c654ffacb39e83d581845964a4b2cf2e02968687
Ucheduk <dukweb4@gmail.com>
2019-11-04 00:10:06 +0100

    add react spiner loader

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,57 +1,24 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React, { ReactNode, Fragment } from "react"
+import React, { Component } from "react"
 import { Helmet } from "react-helmet"
-import Loader from 'react-loader-spinner'
-// import { useStaticQuery, graphql } from "gatsby"
+
 
 import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
-const Layout = ({ children, data }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-  console.log(data);
-  let dom = (
-    <div className="loader-center">
-    <Loader
-         type="Puff"
-         color="#00BFFF"
-         height={100}
-         width={100}
-        //  timeout={3000} //3 secs
-      />
+class Layout extends Component {
+
+  render() {
+    const { children, data } = this.props
+
+    return (
+      <div className="full-height-grow">
+        <Header />
+        {children}
+        <Footer />
       </div>
-  )
-  if (data) {
-      dom = (
-        <div className="full-height-grow">
-          <Header />
-          {children}
-          <Footer />
-        </div>
-      )
-    }
-
-  return (
-    <Fragment>
-    {dom}
-    </Fragment>
-  )
+    )
+  }
 }
 
 export default Layout
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { Link } from 'gatsby';
+import Loader from 'react-loader-spinner'
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import './index.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 let slideIndex = 1;
 
@@ -221,223 +223,242 @@ const IndexPage = ({ data }) => {
       </div>
     );
   });
-  console.log('protfolio: ', portfolios);
+
+  const ex = data.allWordpressPost.edges[0].node.excerpt;
+  const port = data.allWordpressWpPortfolio.edges[0].node.content;
+
+  let dom = (
+    <div className="loader-center">
+    <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        //  timeout={3000} //3 secs
+      />
+      </div>
+  )
+  if (ex && port) {
+      dom = (
+        <Layout data={data}>
+          <SEO title="Home" />
+          <div className="dark-modal">
+            <i
+              onClick={closeVideoModal}
+              className="zmdi zmdi-close close-video"
+            ></i>
+            <div className="video-player"></div>
+          </div>
+          <section className="home-section">
+            <div className="container bio-section">
+              <div className="bio-top">
+                <div className="bio-name">
+                  <div className="bio-name__blue-square"></div>
+                  <h3 className="bio-title">
+                    Honourable
+                    <br />
+                    Ephraim
+                    <br />
+                    Aluebhosele
+                  </h3>
+                  <div className="bio-social">
+                    <i className="zmdi zmdi-facebook bio-social__zmdi"></i>
+                    <i className="zmdi zmdi-twitter bio-social__zmdi"></i>
+                    <i className="zmdi zmdi-instagram bio-social__zmdi"></i>
+                    <i className="zmdi zmdi-linkedin bio-social__zmdi"></i>
+                  </div>
+                </div>
+
+                <div className="bio-image">
+                  <div className="hon-image"></div>
+                </div>
+              </div>
+
+              <div className="bio-bottom">
+                <div className="bio-mission">
+                  <div className="bio-mission__head">
+                    <h3>MISSION</h3>
+                  </div>
+                  <div className="bio-mission__content">
+                    <p>
+                    To adequately represent and empower my constituency and 
+                    support the state in development planning, conflict resolution, 
+                    security, peacemaking and peacebuilding.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="bio-vision">
+                  <div className="bio-vision__head">
+                    <h3>VISION</h3>
+                  </div>
+                  <div className="bio-vision__content">
+                    <p>
+                    Taking Igueben Higher, Joining in Transforming Edo State and NIGERIA
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section className="about-hon-section">
+            <div className="container about-section">
+              <div className="about-image">
+                <div className="hon-image"></div>
+                <h3 className="about-title">
+                  <span className="light-font">About</span>
+                  <br />
+                  Ephraim
+                </h3>
+              </div>
+
+              <div className="about-content">
+                <h3>Ephraim Sele Otaigbe Aluebhosele</h3> Member, Representing
+                Igueben State Constituency at the Edo State House of Assembly -
+                EDHA.
+                <br />
+                <br />
+                <h3>EDUCATION:</h3>
+                University of Ibadan (BA HONS)
+                <br />
+                Liverpool John Moore University, Liverpool. UK. (MBA)
+                <br />
+                <br />
+                <h3>Political Party:</h3>
+                All Progressives Congress (APC)
+                <br />
+                <br />
+                <h3>Political Interest:</h3>
+                Economy,
+                <br />
+                Employment,
+                <br />
+                Small Business,
+                <br />
+                Equality,
+                <br />
+                Education,
+                <br />
+                Agriculture,
+                <br />
+                Justice,
+                <br />
+                Community and Youth Engagement.
+                <br />
+                <br />
+                <h3>House Committees Chairman:</h3> Budget Appriopriation & Project
+                Monitoring.
+                <Link to="/about">Read More</Link>
+              </div>
+            </div>
+          </section>
+
+          {/* Slideshow Portfolio */}
+          <section id="portfolio" className="portfolio-hon-section">
+            <div className="container slideshow-container">
+              <h3 className="portfolio-title">Portfolio</h3>
+
+              {/* Full-width images with number and caption text */}
+              {portfolios}
+              {/* Next and previous buttons */}
+              <a className="prev" onClick={minusSlides}>
+                &#10094;
+              </a>
+              <a className="next" onClick={plusSlides}>
+                &#10095;
+              </a>
+            </div>
+          </section>
+
+          {/* Video */}
+          <section className="container video-section">
+            <div onClick={openVideoModal} className="play-btn">
+              <i className="zmdi zmdi-play video__zmdi"></i>
+            </div>
+            <h3 className="video-title">Watch Video</h3>
+          </section>
+
+          {/* Event */}
+          <section className="event-hon-section">
+            <div className="container event-section">
+              <h3 className="event-title">Upcoming Event</h3>
+
+              <div className="event-content">
+                {events ? events : <p>No Event Created Yet!!!</p>}
+              </div>
+            </div>
+          </section>
+
+          {/* Blog */}
+          <section className="main-blog-hon-section">
+            <div className="container blog-section">
+              <div className="mid-box"></div>
+              <h3 className="main-blog-title">Blog</h3>
+
+              <div className="blog-content">
+                {posts ? posts : <p>No Post Created Yet!!!</p>}
+              </div>
+            </div>
+          </section>
+
+          {/* Contact */}
+          <section id="contact" className="contact-hon-section">
+            <div className="container contact-section">
+              <div className="contact-form">
+                <h3 className="contact-title">Contact Form</h3>
+                <form
+                  className="contact-form__box"
+                  action="POST"
+                  data-netlify="true"
+                >
+                  <label htmlFor="name">What's your name?</label>
+                  <input type="text" name="name" />
+
+                  <label htmlFor="email">What's your email address?</label>
+                  <input type="text" name="email" />
+
+                  <label htmlFor="message">Type your message.</label>
+                  <input type="text" name="message" />
+
+                  <div data-netlify-recaptcha="true"></div>
+
+                  <input
+                    type="submit"
+                    className="contact-form__btn"
+                    value="Send Message"
+                  />
+                </form>
+              </div>
+
+              <div className="contact-content">
+                <h3 className="contact-content__title">
+                  <span className="light-font">Contact</span>
+                  <br />
+                  Hon. Ephraim
+                </h3>
+                <div className="contact-content__details">
+                  <i className="zmdi zmdi-email contact__zmdi"></i>{' '}
+                  info@ephraimseleedha.com
+                </div>
+                <div className="contact-content__details">
+                  <i className="zmdi zmdi-phone contact__zmdi"></i> +234 908 012
+                  3457
+                </div>
+                <div className="contact-content__details">
+                  <i className="zmdi zmdi-pin contact__zmdi"></i> Edo State House of
+                  Assembly, (Igueben Constituency) Anthony Enahoro Complex, 1 King
+                  Square, Benin City.
+                </div>
+              </div>
+            </div>
+          </section>
+        </Layout>
+      )
+    }
 
   return (
-    <Layout data={data}>
-      <SEO title="Home" />
-      <div className="dark-modal">
-        <i
-          onClick={closeVideoModal}
-          className="zmdi zmdi-close close-video"
-        ></i>
-        <div className="video-player"></div>
-      </div>
-      <section className="home-section">
-        <div className="container bio-section">
-          <div className="bio-top">
-            <div className="bio-name">
-              <div className="bio-name__blue-square"></div>
-              <h3 className="bio-title">
-                Honourable
-                <br />
-                Ephraim
-                <br />
-                Aluebhosele
-              </h3>
-              <div className="bio-social">
-                <i className="zmdi zmdi-facebook bio-social__zmdi"></i>
-                <i className="zmdi zmdi-twitter bio-social__zmdi"></i>
-                <i className="zmdi zmdi-instagram bio-social__zmdi"></i>
-                <i className="zmdi zmdi-linkedin bio-social__zmdi"></i>
-              </div>
-            </div>
-
-            <div className="bio-image">
-              <div className="hon-image"></div>
-            </div>
-          </div>
-
-          <div className="bio-bottom">
-            <div className="bio-mission">
-              <div className="bio-mission__head">
-                <h3>MISSION</h3>
-              </div>
-              <div className="bio-mission__content">
-                <p>
-                To adequately represent and empower my constituency and 
-                support the state in development planning, conflict resolution, 
-                security, peacemaking and peacebuilding.
-                </p>
-              </div>
-            </div>
-
-            <div className="bio-vision">
-              <div className="bio-vision__head">
-                <h3>VISION</h3>
-              </div>
-              <div className="bio-vision__content">
-                <p>
-                Taking Igueben Higher, Joining in Transforming Edo State and NIGERIA
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="about-hon-section">
-        <div className="container about-section">
-          <div className="about-image">
-            <div className="hon-image"></div>
-            <h3 className="about-title">
-              <span className="light-font">About</span>
-              <br />
-              Ephraim
-            </h3>
-          </div>
-
-          <div className="about-content">
-            <h3>Ephraim Sele Otaigbe Aluebhosele</h3> Member, Representing
-            Igueben State Constituency at the Edo State House of Assembly -
-            EDHA.
-            <br />
-            <br />
-            <h3>EDUCATION:</h3>
-            University of Ibadan (BA HONS)
-            <br />
-            Liverpool John Moore University, Liverpool. UK. (MBA)
-            <br />
-            <br />
-            <h3>Political Party:</h3>
-            All Progressives Congress (APC)
-            <br />
-            <br />
-            <h3>Political Interest:</h3>
-            Economy,
-            <br />
-            Employment,
-            <br />
-            Small Business,
-            <br />
-            Equality,
-            <br />
-            Education,
-            <br />
-            Agriculture,
-            <br />
-            Justice,
-            <br />
-            Community and Youth Engagement.
-            <br />
-            <br />
-            <h3>House Committees Chairman:</h3> Budget Appriopriation & Project
-            Monitoring.
-            <Link to="/about">Read More</Link>
-          </div>
-        </div>
-      </section>
-
-      {/* Slideshow Portfolio */}
-      <section id="portfolio" className="portfolio-hon-section">
-        <div className="container slideshow-container">
-          <h3 className="portfolio-title">Portfolio</h3>
-
-          {/* Full-width images with number and caption text */}
-          {portfolios}
-          {/* Next and previous buttons */}
-          <a className="prev" onClick={minusSlides}>
-            &#10094;
-          </a>
-          <a className="next" onClick={plusSlides}>
-            &#10095;
-          </a>
-        </div>
-      </section>
-
-      {/* Video */}
-      <section className="container video-section">
-        <div onClick={openVideoModal} className="play-btn">
-          <i className="zmdi zmdi-play video__zmdi"></i>
-        </div>
-        <h3 className="video-title">Watch Video</h3>
-      </section>
-
-      {/* Event */}
-      <section className="event-hon-section">
-        <div className="container event-section">
-          <h3 className="event-title">Upcoming Event</h3>
-
-          <div className="event-content">
-            {events ? events : <p>No Event Created Yet!!!</p>}
-          </div>
-        </div>
-      </section>
-
-      {/* Blog */}
-      <section className="main-blog-hon-section">
-        <div className="container blog-section">
-          <div className="mid-box"></div>
-          <h3 className="main-blog-title">Blog</h3>
-
-          <div className="blog-content">
-            {posts ? posts : <p>No Post Created Yet!!!</p>}
-          </div>
-        </div>
-      </section>
-
-      {/* Contact */}
-      <section id="contact" className="contact-hon-section">
-        <div className="container contact-section">
-          <div className="contact-form">
-            <h3 className="contact-title">Contact Form</h3>
-            <form
-              className="contact-form__box"
-              action="POST"
-              data-netlify="true"
-            >
-              <label htmlFor="name">What's your name?</label>
-              <input type="text" name="name" />
-
-              <label htmlFor="email">What's your email address?</label>
-              <input type="text" name="email" />
-
-              <label htmlFor="message">Type your message.</label>
-              <input type="text" name="message" />
-
-              <div data-netlify-recaptcha="true"></div>
-
-              <input
-                type="submit"
-                className="contact-form__btn"
-                value="Send Message"
-              />
-            </form>
-          </div>
-
-          <div className="contact-content">
-            <h3 className="contact-content__title">
-              <span className="light-font">Contact</span>
-              <br />
-              Hon. Ephraim
-            </h3>
-            <div className="contact-content__details">
-              <i className="zmdi zmdi-email contact__zmdi"></i>{' '}
-              info@ephraimseleedha.com
-            </div>
-            <div className="contact-content__details">
-              <i className="zmdi zmdi-phone contact__zmdi"></i> +234 908 012
-              3457
-            </div>
-            <div className="contact-content__details">
-              <i className="zmdi zmdi-pin contact__zmdi"></i> Edo State House of
-              Assembly, (Igueben Constituency) Anthony Enahoro Complex, 1 King
-              Square, Benin City.
-            </div>
-          </div>
-        </div>
-      </section>
-    </Layout>
+    [dom]
   );
 };
 
